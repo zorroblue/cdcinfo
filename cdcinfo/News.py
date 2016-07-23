eb782ab40e07de8dee1514e7800ce32e46e3ff1b
Rameshwar Bhaskaran <rameshwar.zorro@gmail.com>
2016-07-24 01:11:14 +0530

    Works fine

--- cdcinfo/News.py
+++ cdcinfo/News.py
@@ -1,22 +1,33 @@
 from datetime import datetime
+import sys
 """Represents a single news bulletin"""
+
 
 class News(object):
 
+	sno=""
 	subject=""
 	company=""
 	notice=""
-	date=None
+	date=""
+		
 
-	def convert_date(self,datestring):
-		date = datetime.strptime(str(datestring),'%d-%m-%Y %H:%M')
-
-	def __init__(self,subject,company,notice,datestring):
+	def __init__(self,sno,subject,company,notice,datestring):
+		self.sno=sno
 		self.subject = subject
 		self.company = company
 		self.notice = notice
-		self.convert_date(datestring)
+		try:
+			self.date = datetime.strptime(str(datestring),'%d-%m-%Y %H:%M')
+		except:
+			print "Looks like something is wrong!! Is your config file damaged?"
+			sys.exit()
 
+	def __str__(self):
+
+		matter=str(unicode(self.notice).encode('ascii','ignore'))
+		string = "#######################\nDATE: "+str(self.date)+"\n"+str(self.sno)+str(self.subject)+" ---- "+str(self.company)+"\n\n" +matter+"\n#######################\n"
 	
+		return string
 
 		
--- cdcinfo/cdcparser.py
+++ cdcinfo/cdcparser.py
@@ -2,8 +2,12 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 from News import News
+import datetime
+import traceback
+
 
 base_url = 'http://10.3.100.27/notice/' #The CDC notice page
+colors=["0;31;31","0;30;43","1;30;47","0;37;46"]
 
 def get_page_text():
 	"""Get the entire page source of the CDC notice page"""
@@ -16,6 +20,13 @@ def get_page_text():
 	soup = BeautifulSoup(data,'lxml')
 	return soup
 
+def print_all_results(newsList):
+	i=0
+	for news in newsList:
+		#print news
+		print '\x1b[%sm %s \x1b[0m' %(colors[1],news)
+		i=i+1
+
 def main():
 
 	#soup = get_page_text()
@@ -25,20 +36,67 @@ def main():
 	
 	#parse command line argument
 	arg=None
+	
 	if(len(sys.argv)>1):
 		arg=sys.argv[1]
-		print arg
 	f=open('soup.txt')
 	soup=BeautifulSoup(str(f.read()),'lxml')
 	rows = soup.find("table").findAll('tr')
 	newsList=[]
-	for row in rows[1:]:
+	
+   	for row in rows[1:]:
 		rowList=row.findAll('td')
-		news=News(rowList[1].string,rowList[2].string,rowList[3].string,rowList[4].string)
+		news=News(rowList[0].string,rowList[1].string,rowList[2].string,rowList[3].find("div",class_="noticeHeading").get("data-notif"),rowList[4].string)
 		newsList.append(news)
+	
+	if arg == '--all' or arg == None:
+		print_all_results(newsList)		
 
+	elif arg=='--new':
+		try:
+			f=open('config.txt','r')
+		except:
+			print_all_results(newsList)
+			f.close()
+			f=open('config.txt','w')
+			f.write(datetime.datetime.now().strftime('%d-%m-%Y %H:%M'))
+			sys.exit()
+		data=f.read()
+		if(data is None or len(data)==0):
+			print_all_results(newsList)
+			f.close()
+			f=open('config.txt','w')
+			f.write(datetime.datetime.now().strftime('%d-%m-%Y %H:%M'))
+			sys.exit()
+		#get the last date saved from the file
+		last_date=None
+		try:
+			last_date = datetime.datetime.strptime(data.strip(),'%d-%m-%Y %H:%M')
+		except:
+			print "Looks like something is wrong!!! Is your config file damaged?"
+			traceback.print_exc()
+			sys.exit()
+		
+		if last_date is None:
+			print_all_results(newsList)
+			f.close()
+			f=open('config.txt','w')
+			f.write(datetime.datetime.now().strftime('%d-%m-%Y %H:%M'))
+			sys.exit()
+		else:
+		 	i=0
+			for news in newsList:
+				if(news.date >= last_date):
+					print '\x1b[%sm %s \x1b[0m' %(colors[1],news)
+					i=i+1
+				else:
+					break
+			f.close()
+			f=open('config.txt','w')
+			f.write(datetime.datetime.now().strftime('%d-%m-%Y %H:%M'))
 
-
+	else:
+		print "The usage is cdcinfo --all  [ for printing all notifications ]\n cdcinfo --new  [ for printing the ones you didn't see yet ]"
 
 if __name__=='__main__':
 	main()
