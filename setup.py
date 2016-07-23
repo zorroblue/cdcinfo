8e44e20fd25b28698a97143c80ab9a041ecf5d4e
Rameshwar Bhaskaran <rameshwar.zorro@gmail.com>
2016-07-23 19:00:10 +0530

    Made the basic packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name='cdcinfo',
+        version='1.0',
+        url='http://github.com/zorroblue/cdcinfo',
+        author='Rameshwar Bhaskaran',
+        author_email='rameshwar.zorro@gmail.com',
+        license='MIT',
+        packages=['cdcinfo'],
+        zip_safe=False)
